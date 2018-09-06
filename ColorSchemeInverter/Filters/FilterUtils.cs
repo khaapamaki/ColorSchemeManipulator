28773d8315ebde28c3fddd76e5b42d27706624a2
Kati Haapamäki <kati.haapamaki@valve.fi>
2018-09-06 23:35:22 +0300

    Added type safety for parsing object and strings to doubles

--- ColorSchemeInverter/Filters/FilterUtils.cs
+++ ColorSchemeInverter/Filters/FilterUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Text;
 
 namespace ColorSchemeInverter.Filters
@@ -9,23 +10,33 @@ namespace ColorSchemeInverter.Filters
         public static double GetDouble(object obj)
         {
             if (IsNumber(obj))
-                return (double) obj;
+                try {
+                    return (double) obj;
+                } catch (Exception e) {
+                    Console.WriteLine("Could not cast object to double");
+                }
+
             if (obj is string s)
-                return double.Parse(s);
+                try {
+                    return double.Parse(s);
+                } catch (Exception e) {
+                    Console.WriteLine("Could not parse string to double");
+                }
+
+            Console.WriteLine("Object is not a number, double expected");
             return 0.0;
         }
 
 
         public static bool IsNumber(object o)
         {
-            return o is double || o is decimal || o is int || o is byte || o is float;
+            return o is double || o is decimal || o is int || o is byte || o is float
+                   || o is long || o is short || o is uint || o is ulong || o is ushort;
         }
-        
+
         public static bool IsNumberOrString(object o)
         {
-            return o is double || o is decimal || o is int || o is byte || o is float || o is string;
+            return IsNumber(o) || o is string;
         }
     }
-    
-   
 }
