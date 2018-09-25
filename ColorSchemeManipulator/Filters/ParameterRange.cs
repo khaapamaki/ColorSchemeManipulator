b73eb2fce4cc77f9cf636c7e762c5b2c2eee71f3
Kati Haapamäki <kati.haapamaki@valve.fi>
2018-09-25 09:06:42 +0300

    Todo update

--- ColorSchemeManipulator/Filters/ParameterRange.cs
+++ ColorSchemeManipulator/Filters/ParameterRange.cs
@@ -3,6 +3,8 @@ using ColorSchemeManipulator.Common;
 
 namespace ColorSchemeManipulator.Filters
 {
+    // todo bug fix: slope max is calculated incorrectly
+    
     public class ParameterRange
     {
         public double MinStart { get; set; }
