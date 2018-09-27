290f7af0cb6eec93dab0bf013e5558d6a3adf803
Kati Haapamäki <kati.haapamaki@valve.fi>
2018-09-27 13:50:50 +0300

    Idea xml parsing. WIP

--- ColorSchemeManipulator/SchemeFormats/Handlers/IdeaSchemeFileHandler.cs
+++ ColorSchemeManipulator/SchemeFormats/Handlers/IdeaSchemeFileHandler.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Xml.Linq;
+using ColorSchemeManipulator.Colors;
+
 namespace ColorSchemeManipulator.SchemeFormats.Handlers
 {
     public class IdeaSchemeFileHandler : SchemeFileHandler
@@ -11,13 +18,32 @@ namespace ColorSchemeManipulator.SchemeFormats.Handlers
                 PaddingDirection = PaddingDirection.Left
             }
         };
-        
+
         protected override PaddableHexFormat[] InputHexFormats => _inputHexFormats;
-        
-        protected override string RegexPattern => "<option name=\".+\" value=\"(?<hex>[0-9abcdefABCDEF]{2,6})\"\\s?\\/>";
+
+        protected override string RegexPattern =>
+            "<option name=\".+\" value=\"(?<hex>[0-9abcdefABCDEF]{2,6})\"\\s?\\/>";
 
         protected override string MatchGroupName => "hex";
-        
+
         protected override string OutputHexFormat => "rrggbb";
+
+        public override string ReplaceColors(string text, IEnumerable<Color> colors)
+        {
+            string converterdText = base.ReplaceColors(text, colors);
+//            XElement xElement = XElement.Parse(text);
+//            string schemeParent;
+//
+//            try {
+//                var elements = xElement.Elements();
+//                var test = xElement.Elements().Where(n => n.Name == "scheme").First();
+//                //Console.WriteLine(schemeParent);
+//            } catch (Exception e) {
+//                Console.WriteLine("error");
+//                throw;
+//            }
+
+            return converterdText;
+        }
     }
 }
